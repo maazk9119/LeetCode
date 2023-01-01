9fd32d863ff7338f9947d96df2cd598223f06a2f
maazk9119 <maazk9119@gmail.com>
2023-01-01 14:55:21 +0500

    solved word pattern problem

---
+++ MySolutions/WordPattern/WordPatternClass.cs
@@ -0,0 +1,49 @@
+﻿namespace MySolutions.WordPattern
+{
+    internal class WordPatternClass
+    {
+        public bool Solution(string pattern, string s)
+        {
+            Dictionary<char, string> test = new Dictionary<char, string>();
+            string[] str = s.Split(" ");
+
+            if (pattern.Length != str.Length)
+            {
+                return false;
+            }
+
+            bool result = true;
+
+            for (int i = 0; i < pattern.Length; i++)
+            {
+                if (!test.ContainsKey(pattern[i]))
+                {
+                    foreach (var kv in test)
+                    {
+                        if (kv.Value == str[i])
+                        {
+                            result = false;
+                        }
+                    }
+
+                    if (!result)
+                    {
+                        break;
+                    }
+
+                    test[pattern[i]] = str[i];
+                }
+                else
+                {
+                    if (str[i] != test[pattern[i]])
+                    {
+                        result = false;
+                        break;
+                    }
+                }
+            }
+
+            return result;
+        }
+    }
+}
