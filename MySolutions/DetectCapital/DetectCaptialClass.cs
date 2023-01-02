7f93e504f24672cc0fac9fc0b2ec3e7b79196cb5
maazk9119 <maazk9119@gmail.com>
2023-01-02 14:04:24 +0500

    solved captial detect problem

---
+++ MySolutions/DetectCapital/DetectCaptialClass.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MySolutions.DetectCapital
+{
+    internal class DetectCaptialClass
+    {
+        public static bool Solution(string word)
+        {
+            if (word.ToUpper() == word || word.ToLower() == word)
+            {
+                return true;
+            }
+
+            if (Char.IsUpper(word[0]))
+            {
+                for (int i = 1; i < word.Length; i++)
+                {
+                    if (Char.IsUpper(word[i]))
+                    {
+                        return false;
+                    }
+                }
+
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
