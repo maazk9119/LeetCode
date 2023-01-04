54be9ddb8efb723674444c0d2b683aae902d15b5
maazk9119 <maazk9119@gmail.com>
2023-01-04 16:50:59 +0500

    solved two sum problem

---
+++ MySolutions/TwoSum/TwoSumClass.cs
@@ -0,0 +1,27 @@
+﻿namespace MySolutions.TwoSum
+{
+    internal class TwoSumClass
+    {
+        public static int[] Solution(int[] nums, int target)
+        {
+            List<int> numbers = nums.ToList();
+            int[] index = new int[2];
+
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                int notFound = target - numbers[i];
+                int index2 = numbers.IndexOf(notFound, i + 1);
+
+                if (index2 != -1)
+                {
+                    index[0] = i;
+                    index[1] = index2;
+
+                }
+            }
+
+
+            return index;
+        }
+    }
+}
