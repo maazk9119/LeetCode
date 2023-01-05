53aeabf1749fcce8a1ab67b46ce450511d9561ff
maazk9119 <maazk9119@gmail.com>
2023-01-05 23:05:26 +0500

    solved minimum number of arrows to brust balloons  - medium level

---
+++ MySolutions/452.MinimumArrowsToBrustBallons/MinimumArrows.cs
@@ -0,0 +1,22 @@
+﻿namespace MySolutions._452.MinimumArrowsToBrustBallons
+{
+    internal class MinimumArrows
+    {
+        public static int FindMinArrowShots(int[][] points)
+        {
+            Array.Sort(points, (a, b) => a[1].CompareTo(b[1]));
+      
+            int end = points[0][1], arrow = 1;
+            for (int i = 1; i < points.Length; i++)
+            {
+                if (end >= points[i][0])
+                    continue;
+
+                arrow++;
+                end = points[i][1];
+            }
+
+            return arrow;
+        }
+    }
+}
