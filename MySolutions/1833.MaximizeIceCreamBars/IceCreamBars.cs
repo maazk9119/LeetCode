ac50311b0153a91cd38d0bed9fc82a305d6037b1
maazk9119 <maazk9119@gmail.com>
2023-01-06 15:03:09 +0500

    solved maximize ice cream bars  - medium level

---
+++ MySolutions/1833.MaximizeIceCreamBars/IceCreamBars.cs
@@ -0,0 +1,26 @@
+﻿namespace MySolutions._1833.MaximizeIceCreamBars
+{
+    internal class IceCreamBars
+    {
+        public int MaxIceCream(int[] costs, int coins)
+        {
+            Array.Sort(costs);
+
+            int bar = 0;
+            for (int i = 0; i < costs.Length; i++)
+            {
+                if (coins > 0 && costs[i] <= coins)
+                {
+                    coins -= costs[i];
+                    bar++;
+                    continue;
+                }
+
+                break;
+            }
+
+
+            return bar;
+        }
+    }
+}
