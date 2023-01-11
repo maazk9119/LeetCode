c2b57a852f3c40b7b0a3a543c5f74316f94e8129
maazk9119 <maazk9119@gmail.com>
2023-01-11 20:30:56 +0500

    solved same tree problem - easy level

---
+++ MySolutions/100.SameTree/BSTTree.cs
@@ -0,0 +1,35 @@
+﻿namespace MySolutions._100.SameTree
+{
+    public class TreeNode
+    {
+       public int val;
+       public TreeNode left;
+       public TreeNode right;
+       
+        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    internal class BSTTree
+    {
+        public bool IsSameTree(TreeNode p, TreeNode q)
+        {
+            return Validate(p, q);
+        }
+
+        private bool Validate(TreeNode p, TreeNode q)
+        {
+            if (p == null && q != null) return false;
+            if (p != null && q == null) return false;
+            if (p == null && q == null) return true;
+
+            if (p.val != q.val) return false;
+
+            return Validate(p.left, q.left) && Validate(p.right, q.right);
+        }
+    }
+}
