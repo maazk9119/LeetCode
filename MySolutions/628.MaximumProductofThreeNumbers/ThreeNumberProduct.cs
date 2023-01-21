e3da0997ab8706d2b4d0b429cd21dde142d31c72
maazk9119 <maazk9119@gmail.com>
2023-01-21 19:31:18 +0500

    solved maximum product of three numbers  - easy level

---
+++ MySolutions/628.MaximumProductofThreeNumbers/ThreeNumberProduct.cs
@@ -0,0 +1,15 @@
+﻿namespace MySolutions._628.MaximumProductofThreeNumbers
+{
+    internal class ThreeNumberProduct
+    {
+        public int MaximumProduct(int[] nums)
+        {
+            Array.Sort(nums);
+
+            int i = nums[0] * nums[1] * nums[nums.Length - 1];
+            int j = nums[nums.Length - 1] * nums[nums.Length - 2] * nums[nums.Length - 3];
+
+            return i > j ? i : j;
+        }
+    }
+}
