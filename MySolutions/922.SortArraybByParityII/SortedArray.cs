80ef3abda5317b2faf576de242e0d435a6c53731
maazk9119 <maazk9119@gmail.com>
2023-01-21 19:20:53 +0500

    solved sort array by parity II  - easy level

---
+++ MySolutions/922.SortArraybByParityII/SortedArray.cs
@@ -0,0 +1,27 @@
+﻿namespace MySolutions._922.SortArraybByParityII
+{
+    internal class SortedArray
+    {
+        public int[] SortArrayByParityII(int[] nums)
+        {
+            int i = 0;
+            int j = 1;
+            int[] result = new int[nums.Length];
+
+            for (int p = 0; p < nums.Length; p++)
+            {
+                if (nums[p] % 2 == 0)
+                {
+                    result[i] = nums[p];
+                    i = i + 2;
+                    continue;
+                }
+
+                result[j] = nums[p];
+                j = j + 2;
+            }
+
+            return result;
+        }
+    }
+}
