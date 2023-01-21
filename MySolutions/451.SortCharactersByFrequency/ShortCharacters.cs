658774071008a88b5301681b386dd5c1267f8d85
maazk9119 <maazk9119@gmail.com>
2023-01-21 19:06:21 +0500

    solved sort characters by frequency  - medium level

---
+++ MySolutions/451.SortCharactersByFrequency/ShortCharacters.cs
@@ -0,0 +1,31 @@
+﻿using System.Text;
+
+namespace MySolutions._451.SortCharactersByFrequency
+{
+    internal class ShortCharacters
+    {
+        public string FrequencySort(string s)
+        {
+            StringBuilder result = new StringBuilder();
+            Dictionary<char, int> frequency = new Dictionary<char, int>();
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (!frequency.ContainsKey(s[i]))
+                {
+                    frequency.Add(s[i], 1);
+                    continue;
+                }
+
+                frequency[s[i]] += 1;
+            }
+
+            foreach (KeyValuePair<char, int> fre in frequency.OrderByDescending(x => x.Value))
+            {
+                result.Append(fre.Key, fre.Value);
+            }
+
+            return result.ToString();
+        }
+    }
+}
