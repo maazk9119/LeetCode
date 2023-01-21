3854c523465766f3dd8c0c061a0aa2c267d0648c
maazk9119 <maazk9119@gmail.com>
2023-01-21 19:25:16 +0500

    solved best time to buy and sell stock II  - medium level

---
+++ MySolutions/112.BestTimeToBuyAndSellStoackII/BuyAndSellStock.cs
@@ -0,0 +1,23 @@
+﻿namespace MySolutions._112.BestTimeToBuyAndSellStoackII
+{
+    internal class BuyAndSellStock
+    {
+        public int MaxProfit(int[] prices)
+        {
+            int totalProfit = 0;
+
+            for (int i = 1; i < prices.Length; i++)
+            {
+                int buy = prices[i - 1];
+                int profit = prices[i] - buy;
+
+                if (profit > 0)
+                {
+                    totalProfit += profit;
+                }
+            }
+
+            return totalProfit;
+        }
+    }
+}
