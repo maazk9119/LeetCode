e58641f6f62228ea5bec61aa8ac7a1b62222fd47
maazk9119 <maazk9119@gmail.com>
2023-01-21 19:15:04 +0500

    solved search insert position  - easy level

---
+++ MySolutions/35.SearchInsertPosition/InsertPosition.cs
@@ -0,0 +1,20 @@
+﻿namespace MySolutions._35.SearchInsertPosition
+{
+    internal class InsertPosition
+    {
+        public int SearchInsert(int[] nums, int target)
+        {
+            List<int> list = nums.ToList();
+
+            if (!list.Contains(target))
+            {
+                list.Add(target);
+                list.Sort();
+
+                return list.IndexOf(target);
+            }
+
+            return list.IndexOf(target);
+        }
+    }
+}
