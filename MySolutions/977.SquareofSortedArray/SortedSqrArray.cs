19c2ba7d39d25b96538b6dbc732a345947b724f2
maazk9119 <maazk9119@gmail.com>
2023-01-22 12:35:28 +0500

    solved squre sorted array  - easy level

---
+++ MySolutions/977.SquareofSortedArray/SortedSqrArray.cs
@@ -0,0 +1,17 @@
+﻿namespace MySolutions._977.SquareofSortedArray
+{
+    internal class SortedSqrArray
+    {
+        public int[] SortedSquares(int[] nums)
+        {
+            for (int i = 0; i < nums.Length; i++)
+            {
+                nums[i] = nums[i] * nums[i];
+            }
+
+            Array.Sort(nums);
+
+            return nums;
+        }
+    }
+}
