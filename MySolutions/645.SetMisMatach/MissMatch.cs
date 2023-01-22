e24bd53688e343fc89878379355e04438bc1cd98
maazk9119 <maazk9119@gmail.com>
2023-01-22 13:35:42 +0500

    solved set miss-match problem  - easy level

---
+++ MySolutions/645.SetMisMatach/MissMatch.cs
@@ -0,0 +1,25 @@
+﻿namespace MySolutions._645.SetMisMatach
+{
+    internal class MissMatch
+    {
+        public int[] FindErrorNums(int[] nums)
+        {
+            int element = 0; //missing element 
+            int duplicateIndex = -1;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (duplicateIndex == -1)
+                {
+                    duplicateIndex = nums.ToList().IndexOf(nums[i], i + 1);
+                }
+                if (!nums.Contains(i + 1))
+                {
+                    element = i + 1;
+                }
+            }
+
+            return new int[] { nums[duplicateIndex], element };
+        }
+    }
+}
