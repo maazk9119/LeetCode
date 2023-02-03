1ead400409a43647de21e2a626fab977a52c5028
maazk9119 <maazk9119@gmail.com>
2023-02-03 20:34:01 +0500

    rename

--- MySolutions/520.DetectCapital/DetectCaptialClass.cs
+++ MySolutions/520.DetectCapital/DetectCaptialClass.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace MySolutions.DetectCapital
+﻿namespace MySolutions._520.DetectCapital
 {
     internal class DetectCaptialClass
     {
