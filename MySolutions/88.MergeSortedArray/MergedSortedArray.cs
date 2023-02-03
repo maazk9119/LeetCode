ca860b8e062c6a82266027db7081e829714c9a96
maazk9119 <maazk9119@gmail.com>
2023-02-03 20:31:18 +0500

    solved merged sorted arrary  - easy level

---
+++ MySolutions/88.MergeSortedArray/MergedSortedArray.cs
@@ -0,0 +1,21 @@
+﻿namespace MySolutions._88.MergeSortedArray
+{
+    internal class MergedSortedArray
+    {
+        public void Merge(int[] nums1, int m, int[] nums2, int n)
+        {
+            int count = 0;
+
+            for (int i = nums1.Length - 1; i >= 0; i--)
+            {
+                if (nums1[i] == 0 && count < nums2.Length)
+                {
+                    nums1[i] = nums2[count];
+                    count++;
+                }
+            }
+
+            Array.Sort(nums1);
+        }
+    }
+}
