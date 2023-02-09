4c5a0e0e4bbd9bc34ed67b6e74d4dd03c981df59
maazk9119 <maazk9119@gmail.com>
2023-02-09 16:36:50 +0500

    solved minimum depth of binary tree  -easy level

---
+++ MySolutions/111.MinimumDepthOfBinaryTree/MiniDepthBTree.cs
@@ -0,0 +1,27 @@
+﻿namespace MySolutions._111.MinimumDepthOfBinaryTree
+{
+    internal class MiniDepthBTree
+    {
+        public int MinDepth(TreeNode root)
+        {
+            return DFS(root);
+        }
+
+        private int DFS(TreeNode node)
+        {
+            if (node == null)
+                return 0;
+
+            else if (node.left == null)
+                return DFS(node.right) + 1;
+
+            else if (node.right == null)
+                return DFS(node.left) + 1;
+
+            int leftH = DFS(node.left);
+            int rightH = DFS(node.right);
+
+            return leftH > rightH ? rightH + 1 : leftH + 1;
+        }
+    }
+}
