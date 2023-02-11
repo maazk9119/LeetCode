5d181fa7ff734f9f00eb0e68725c11691705aaf0
maazk9119 <maazk9119@gmail.com>
2023-02-11 15:07:59 +0500

    solved maximum binary search tree depth  - easy level

---
+++ MySolutions/104.MaximumDepthofBST/MaxBSTDepth.cs
@@ -0,0 +1,25 @@
+﻿namespace MySolutions._104.MaximumDepthofBST
+{
+    internal class MaxBSTDepth
+    {
+        public int MaxDepth(TreeNode root)
+        {
+            if (root == null)
+            {
+                return 0;
+            }
+
+            return CalculateDepth(root);
+        }
+
+        private int CalculateDepth(TreeNode root)
+        {
+            if (root == null) return 0;
+
+            int left = CalculateDepth(root.left);
+            int right = CalculateDepth(root.right);
+
+            return left > right ? left + 1 : right + 1;
+        }
+    }
+}
