673a537770db30dd2638ab87e65ed20a2d498415
maazk9119 <maazk9119@gmail.com>
2023-02-11 15:18:23 +0500

    solved path sum problem  - easy level

---
+++ MySolutions/112.PathSum/SumBTPath.cs
@@ -0,0 +1,17 @@
+﻿namespace MySolutions._112.PathSum
+{
+    internal class SumBTPath
+    {
+        public bool HasPathSum(TreeNode root, int targetSum)
+        {
+            if (root is null)
+                return false;
+
+            if (root.left == null && root.right == null)
+                return root.val == targetSum;
+
+            return HasPathSum(root.left, targetSum - root.val) ||
+                   HasPathSum(root.right, targetSum - root.val);
+        }
+    }
+}
