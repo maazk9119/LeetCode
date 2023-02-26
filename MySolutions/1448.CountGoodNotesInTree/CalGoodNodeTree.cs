6131b75921b1d39850443c18dddcbc323cacd716
maazk9119 <maazk9119@gmail.com>
2023-02-26 16:37:37 +0500

    solved count good nodes in binary tree  -medium level

---
+++ MySolutions/1448.CountGoodNotesInTree/CalGoodNodeTree.cs
@@ -0,0 +1,29 @@
+﻿namespace MySolutions._1448.CountGoodNotesInTree
+{
+    internal class CalGoodNodeTree
+    {
+        public int GoodNodes(TreeNode root)
+        {
+            return numberOfGoodNodes(root, root.val);
+        }
+
+        public int numberOfGoodNodes(TreeNode root, int min)
+        {
+            //base
+            if (root is null)
+            {
+                return 0;
+            }
+
+            int max = root.val >= min ? root.val : min;
+
+            int goodNode = 0;
+            if (root.val >= min)
+            {
+                goodNode += 1;
+            }
+
+            return numberOfGoodNodes(root.left, max) + numberOfGoodNodes(root.right, max) + goodNode;
+        }
+    }
+}
