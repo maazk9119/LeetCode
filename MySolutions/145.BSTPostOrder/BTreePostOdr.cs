bc3894ecd7c8bd794393f560efc6ea0a3180f082
maazk9119 <maazk9119@gmail.com>
2023-02-26 16:56:45 +0500

    solved binary tree post order traversal  -easy level

---
+++ MySolutions/145.BSTPostOrder/BTreePostOdr.cs
@@ -0,0 +1,23 @@
+﻿namespace MySolutions._145.BSTPostOrder
+{
+    internal class BTreePostOdr
+    {
+        public IList<int> PostorderTraversal(TreeNode root)
+        {
+            IList<int> list = new List<int>(); //save the result
+            Traverse(root, list);
+
+            return list;
+        }
+
+        public void Traverse(TreeNode node, IList<int> result)
+        {
+            if (node is null)
+                return;
+
+            Traverse(node.left, result);
+            Traverse(node.right, result);
+            result.Add(node.val);
+        }
+    }
+}
